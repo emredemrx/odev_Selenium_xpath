41edd5d14020d7fd7a7fe6260dbb447cf64bb8a5
emred <femredemir699@gmail.com>
2024-08-06 23:05:08 +0300

    odev 15

--- src/test/java/test/demoTest.java
+++ src/test/java/test/demoTest.java
@@ -1,5 +1,5 @@
-package org.test;
-import com.beust.ah.A;
+package test;
+
 import jdk.jfr.Description;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -22,29 +21,15 @@ public class demoTest {
     @BeforeClass
     public void setup(){
         System.setProperty("webdriver.chrome.driver", "src/main/java/drivers/chromedriver.exe");
-
         ChromeOptions options = new ChromeOptions();
-        // options.addArguments("--headless");
         options.addArguments("--window-size=1920,1080");
         options.addArguments("--disable-extensions");
         options.addArguments("--disable-gpu");
         options.addArguments("--ignore-certificate-errors");
-        // options.addArguments("--incognito");  gizli sekmede işlem yapmak istediğimizde
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--start-maximized");
-
         driver = new ChromeDriver(options);
-
-        //page load timeout
-        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-
-        //imlicit wait
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-
-        //explicit wait
-        //wait =new WebDriverWait(driver, Duration.ofSeconds(10));
-
     }
 
     @Test @Description("Test - Click Button")
@@ -52,13 +37,13 @@ public class demoTest {
         driver.get("https://demoqa.com/elements");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 
-        WebElement btnClick = driver.findElement(By.cssSelector("#item-4"));
+        WebElement btnClick = driver.findElement(By.xpath("//li[.='Buttons']"));
         btnClick.click();
 
-        WebElement btnClick2 = driver.findElement(By.cssSelector(".col-md-6 div:nth-of-type(3) > .btn"));
+        WebElement btnClick2 = driver.findElement(By.xpath("//button[text()='Click Me']"));
         btnClick2.click();
 
-        WebElement text = driver.findElement(By.cssSelector("#dynamicClickMessage"));
+        WebElement text = driver.findElement(By.xpath("//p[@id='dynamicClickMessage']"));
         Assert.assertTrue(text.isDisplayed());
         Assert.assertEquals(text.getText(),"You have done a dynamic click");
     }
@@ -67,41 +52,36 @@ public class demoTest {
     public void addRecord (){
         driver.get("https://demoqa.com/webtables");
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-
-        WebElement addClick = driver.findElement(By.cssSelector("#addNewRecordButton"));
+        WebElement addClick = driver.findElement(By.xpath("//button[@id='addNewRecordButton']"));
         addClick.click();
-
-        WebElement firstNameInput = driver.findElement(By.cssSelector("#firstName"));
+        WebElement firstNameInput = driver.findElement(By.xpath("//input[@id='firstName']"));
         firstNameInput.sendKeys("Emre");
-        WebElement lastNameInput = driver.findElement(By.cssSelector("#lastName"));
+        WebElement lastNameInput = driver.findElement(By.xpath("//input[@id='lastName']"));
         lastNameInput.sendKeys("Demir");
-        WebElement eMailInput = driver.findElement(By.cssSelector("#userEmail"));
+        WebElement eMailInput = driver.findElement(By.xpath("//input[@id='userEmail']"));
         eMailInput.sendKeys("emre@emre.com");
-        WebElement ageInput = driver.findElement(By.cssSelector("#age"));
+        WebElement ageInput = driver.findElement(By.xpath("//input[@id='age']"));
         ageInput.sendKeys("30");
-        WebElement SalaryInput = driver.findElement(By.cssSelector("#salary"));
+        WebElement SalaryInput = driver.findElement(By.xpath("//input[@id='salary']"));
         SalaryInput.sendKeys("40000");
-        WebElement departmentInput = driver.findElement(By.cssSelector("#department"));
+        WebElement departmentInput = driver.findElement(By.xpath("//input[@id='department']"));
         departmentInput.sendKeys("İK");
-        WebElement submitClick = driver.findElement(By.cssSelector("#submit"));
+        WebElement submitClick = driver.findElement(By.xpath("//button[@id='submit']"));
         submitClick.click();
-        WebElement updateClick = driver.findElement(By.cssSelector(".rt-tbody > div:nth-of-type(4) .mr-2"));
+        WebElement updateClick = driver.findElement(By.xpath("//div[@class='rt-tbody']/div[4]//span[@class='mr-2']"));
         updateClick.click();
-        WebElement SalaryUpdateInput = driver.findElement(By.cssSelector("#salary"));
+        WebElement SalaryUpdateInput = driver.findElement(By.xpath("//input[@id='salary']"));
         SalaryUpdateInput.clear();
         SalaryUpdateInput.sendKeys("60000");
-        WebElement departmentUpdateInput = driver.findElement(By.cssSelector("#department"));
+        WebElement departmentUpdateInput = driver.findElement(By.xpath("//input[@id='department']"));
         departmentUpdateInput.clear();
         departmentUpdateInput.sendKeys("insan kaynakları");
-        WebElement submitUpdateClick = driver.findElement(By.cssSelector("#submit"));
+        WebElement submitUpdateClick = driver.findElement(By.xpath("//button[@id='submit']"));
         submitUpdateClick.click();
-
-
-        WebElement SalaryUpdateControl = driver.findElement(By.cssSelector(".rt-tbody > div:nth-of-type(4) div:nth-of-type(5)"));
+        WebElement SalaryUpdateControl = driver.findElement(By.xpath("//div[@class='rt-tr-group']/following::div[27]"));
         Assert.assertTrue(SalaryUpdateControl.isDisplayed());
         Assert.assertEquals(SalaryUpdateControl.getText(),"60000");
-
-        WebElement departmentUpdateControl = driver.findElement(By.cssSelector(".rt-tbody > div:nth-of-type(4) div:nth-of-type(6)"));
+        WebElement departmentUpdateControl = driver.findElement(By.xpath("//div[@class='rt-tr-group']/following::div[28]"));
         Assert.assertTrue(departmentUpdateControl.isDisplayed());
         Assert.assertEquals(departmentUpdateControl.getText(),"insan kaynakları");
     }
